13e61956e24c5032a879d440c0573eafb27abc10
Sebastian Flajszer <sebastianflajszer@gmail.com>
2020-09-07 21:10:25 +0200

    twitter icon fix / added disabled button style

--- src/components/repositoryCard.vue
+++ src/components/repositoryCard.vue
@@ -2,7 +2,11 @@
   <article class="repository mt">
     <header class="header">
       <span>{{ repository.name }}</span>
-      <a :href="repository.html_url" class="header__subtitle --align-vertical" target="_blank">
+      <a
+        :href="repository.html_url"
+        class="header__subtitle --align-vertical"
+        target="_blank"
+      >
         <img src="@/assets/icons/link.svg" alt="Link image" class="icon" />
         <span>{{ repository.full_name }}</span>
       </a>
@@ -21,7 +25,11 @@
         </li>
         <span class="breadcrumb__item--separator">&middot;</span>
         <li class="breadcrumb__item" title="forks">
-          <img class="icon" src="@/assets/icons/code-fork.svg" alt="Code fork icon" />
+          <img
+            class="icon"
+            src="@/assets/icons/code-fork.svg"
+            alt="Code fork icon"
+          />
           <span>{{ formatNumber(repository.forks_count) }}</span>
         </li>
       </ul>
--- src/components/userCard.vue
+++ src/components/userCard.vue
@@ -1,6 +1,10 @@
 <template>
   <article class="profile">
-    <img class="profile__image" :src="user.avatar_url" alt="Avatar of github account" />
+    <img
+      class="profile__image"
+      :src="user.avatar_url"
+      alt="Avatar of github account"
+    />
     <header class="header">
       <span class="header__title">{{ user.login }}</span>
       <span class="header__subtitle">{{ user.name }}</span>
@@ -11,32 +15,56 @@
         Check on Github
       </a>
       <ul class="breadcrumb mt">
-        <li class="breadcrumb__item">{{ formatNumber(user.followers) }} followers</li>
+        <li class="breadcrumb__item">
+          {{ formatNumber(user.followers) }} followers
+        </li>
         <span class="breadcrumb__item--separator">&middot;</span>
-        <li class="breadcrumb__item">{{ formatNumber(user.following) }} following</li>
+        <li class="breadcrumb__item">
+          {{ formatNumber(user.following) }} following
+        </li>
         <span class="breadcrumb__item--separator">&middot;</span>
-        <li class="breadcrumb__item">{{ formatNumber(user.public_repos) }} repos</li>
+        <li class="breadcrumb__item">
+          {{ formatNumber(user.public_repos) }} repos
+        </li>
       </ul>
       <ul class="info mt">
         <li class="info__item" v-if="user.company">
-          <img class="icon" src="@/assets/icons/company.svg" alt="Building icon" />
+          <img
+            class="icon"
+            src="@/assets/icons/company.svg"
+            alt="Building icon"
+          />
           <span>{{ user.company }}</span>
         </li>
         <li class="info__item" v-if="user.location">
-          <img class="icon" src="@/assets/icons/location.svg" alt="Location icon" />
+          <img
+            class="icon"
+            src="@/assets/icons/location.svg"
+            alt="Location icon"
+          />
           <span>{{ user.location }}</span>
         </li>
         <li class="info__item" v-if="user.blog">
-          <img class="icon" src="@/assets/icons/blog.svg" alt="Blog page icon" />
+          <img
+            class="icon"
+            src="@/assets/icons/blog.svg"
+            alt="Blog page icon"
+          />
           <a :href="user.blog" target="_blank">{{ user.blog }}</a>
         </li>
         <li class="info__item" v-if="user.email">
           <img class="icon" src="@/assets/icons/mail.svg" alt="Mail icon" />
           <a :href="`mailto: ${user.email}`">{{ user.email }}</a>
         </li>
-        <li class="info_item" v-if="user.twitter_username">
-          <img class="icon" src="@/assets/icons/twitter.svg" alt="Twitter icon" />
-          <a :href="`https://twitter.com/${user.twitter_username}`">@{{ user.twitter_username }}</a>
+        <li class="info__item" v-if="user.twitter_username">
+          <img
+            class="icon"
+            src="@/assets/icons/twitter.svg"
+            alt="Twitter icon"
+          />
+          <a :href="`https://twitter.com/${user.twitter_username}`"
+            >@{{ user.twitter_username }}</a
+          >
         </li>
       </ul>
     </main>
